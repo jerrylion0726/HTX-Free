623a02c221cc69925c080bbc55b5efd43b60beeb
Jerry Chen <chenyuming726@gmail.com>
2026-07-28 13:44:57 -0500

    Initial Commit

---
+++ PublicResourceFinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PublicResourceFinder
+//
+//  Created by 陈禹铭 on 7/28/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PublicResourceFinder/PublicResourceFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PublicResourceFinderApp.swift
+//  PublicResourceFinder
+//
+//  Created by 陈禹铭 on 7/28/26.
+//
+
+import SwiftUI
+
+@main
+struct PublicResourceFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
